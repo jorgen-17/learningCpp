0ada34f1d5f1c0dfaeed2fa03c9c728092ac938e
typedefJorge <jnone001@fiu.edu>
2016-10-24 21:29:07 -0400

    changed up structure of test dependencies

--- artOfProgramming/euclid/euclidTests.cpp
+++ artOfProgramming/euclid/euclidTests.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "euclid.hpp"
-#include "catch.hpp"
+#include "../../dependencies/Catch/single_include/catch.hpp"
 
 
 TEST_CASE("Euclid function returns correct GCDs", "[euclid]") {
